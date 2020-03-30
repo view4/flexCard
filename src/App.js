3c05417f705cd3920cd88fe7d1776c58581f2d32
Gabriel Morris <morrisgaby987@googlemail.com>
2020-03-30 14:45:42 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FlexCard from "./FlexCard";
+
+function App() {
+  return (
+    <div className="App">
+	<FlexCard />
+	<FlexCard />
+	<FlexCard />
+	<FlexCard />
+	<FlexCard />
+	<FlexCard />
+	<FlexCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FlexCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const FlexCard = () => {
+
+
+  return(
+	<div className="card-container">
+		<div className="image-container">
+		</div>
+		<div className="content-container">
+			<div className="title-container">
+				<h4>
+					This Building: 
+				</h4>
+			</div>
+			<div className="description-container">
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+
+				</p>
+			</div>
+		</div>
+        </div>
+
+  )
+};
+
+export default FlexCard
